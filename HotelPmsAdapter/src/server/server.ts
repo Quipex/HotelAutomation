f14b661737b184783ef317d6ff3b99af902f3a39
DmytroR <rudas72@gmail.com>
2022-05-24 21:19:53 +0300

    Modified database events handling

--- HotelPmsAdapter/src/server/server.ts
+++ HotelPmsAdapter/src/server/server.ts
@@ -1,5 +1,6 @@
 import Application from 'koa';
 import bodyParser from 'koa-bodyparser';
+import { ErrorCode } from '~/common/constants';
 import appDataSource from '~/config/dataSource';
 import env from '~/config/env';
 import { log } from '~/config/logger';
@@ -14,8 +15,16 @@ app.use(trackResponseTime);
 app.use(logRequestAndResponseTime);
 app.on('error', handleErrors);
 app.use(appRouter.routes());
+app.on('close', async () => {
+  try {
+    await appDataSource.destroy();
+  } catch (e) {
+    log.error('Failed to destroy db connection', e);
+    process.exit(ErrorCode.DATABASE);
+  }
+});
 
-appDataSource.initialize().then(async connection => {
+appDataSource.initialize().then(async (connection) => {
   const connectionOpts = connection.options;
   log.info('A connection to database is created', {
     vendor: connectionOpts.type,
@@ -24,9 +33,11 @@ appDataSource.initialize().then(async connection => {
   });
 
   app.listen(env.port, () => {
-    log.info(`⚡️[server]: Server is running on port ${env.port}`);
+    log.info(`⚡️[server]: Server is running on port ${env.port}. Environment: ${env.nodeEnv}`);
   });
 }).catch((err) => {
   log.error('Error while creating db connection', err);
-  process.exit(1);
+  process.exit(ErrorCode.DATABASE);
 });
+
+export default app;
