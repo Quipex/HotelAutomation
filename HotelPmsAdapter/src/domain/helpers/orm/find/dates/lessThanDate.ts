b3e105f8419888cc47461ee4d5e0690ce4ed63a0
DmytroR <rudas72@gmail.com>
2022-05-24 21:15:35 +0300

    Added date db comparators

--- HotelPmsAdapter/src/domain/helpers/orm/find/dates/lessThanDate.ts
+++ HotelPmsAdapter/src/domain/helpers/orm/find/dates/lessThanDate.ts
@@ -1,5 +1,5 @@
-import { formatDate } from '~/common/utils/dates';
 import { Raw } from 'typeorm';
+import { formatDate } from '~/common/utils/dates';
 
 const lessThanDate = (date: Date) => {
   return Raw((alias) => `${alias} < :date`, { date: formatDate(date) });
--- HotelPmsAdapter/src/domain/helpers/orm/find/dates/moreThanDate.ts
+++ HotelPmsAdapter/src/domain/helpers/orm/find/dates/moreThanDate.ts
@@ -1,5 +1,5 @@
-import { formatDate } from '~/common/utils/dates';
 import { Raw } from 'typeorm';
+import { formatDate } from '~/common/utils/dates';
 
 const moreThanDate = (date: Date) => {
   return Raw((alias) => `${alias} > :date`, { date: formatDate(date) });
