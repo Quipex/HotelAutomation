cbeed26b2a502e12360cd1f90bd8aff079bdcad3
DmytroR <rudas72@gmail.com>
2022-06-06 00:11:34 +0300

    fix

--- HotelPmsAdapter/tests/jestSetup.ts
+++ HotelPmsAdapter/tests/jestSetup.ts
@@ -3,19 +3,11 @@ import { testsLog } from '~/config/logger';
 
 beforeAll(async () => {
   testsLog.info('Before tests');
-  try {
-    await appDataSource.initialize();
-  } catch (e) {
-    process.exit(1);
-  }
+  await appDataSource.initialize();
 });
 
 afterAll(async () => {
   testsLog.info('After tests');
-  try {
-    await appDataSource.undoLastMigration({ transaction: 'all' });
-    await appDataSource.destroy();
-  } catch (e) {
-    process.exit(1);
-  }
+  await appDataSource.undoLastMigration({ transaction: 'all' });
+  await appDataSource.destroy();
 });
