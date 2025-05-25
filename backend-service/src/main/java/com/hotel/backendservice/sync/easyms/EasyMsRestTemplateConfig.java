a889137391d35e381a459d53f61bfb3e98754ce1
Quipex <rudas72@gmail.com>
2025-05-25 21:11:58 +0200

    checkpoint: remove tests for EasyMsRestTemplateConfig

--- backend-service/src/main/java/com/hotel/backendservice/sync/easyms/EasyMsRestTemplateConfig.java
+++ backend-service/src/main/java/com/hotel/backendservice/sync/easyms/EasyMsRestTemplateConfig.java
@@ -1,20 +1,18 @@
 package com.hotel.backendservice.sync.easyms;
 
+import io.github.resilience4j.retry.Retry;
 import io.github.resilience4j.retry.RetryConfig;
 import io.github.resilience4j.retry.RetryRegistry;
-import io.github.resilience4j.retry.Retry;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
 
