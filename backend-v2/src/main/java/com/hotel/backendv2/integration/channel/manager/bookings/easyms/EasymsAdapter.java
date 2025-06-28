eedced3b69f6f9d754c86d4fb43459cf582cd5c8
Quipex <rudas72@gmail.com>
2025-06-28 20:29:54 +0200

    Implement EasymsAdapter with RestTemplate integration for booking fetching

--- backend-v2/src/main/java/com/hotel/backendv2/integration/channel/manager/bookings/easyms/EasymsAdapter.java
+++ backend-v2/src/main/java/com/hotel/backendv2/integration/channel/manager/bookings/easyms/EasymsAdapter.java
@@ -2,11 +2,21 @@ package com.hotel.backendv2.integration.channel.manager.bookings.easyms;
 
 import com.hotel.backendv2.integration.channel.manager.bookings.ChannelManagerAdapter;
 import com.hotel.backendv2.integration.channel.manager.bookings.CmBookingDto;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Component
 public class EasymsAdapter implements ChannelManagerAdapter {
+    private final RestTemplate client;
+
+    public EasymsAdapter(@Qualifier("easyms-client") RestTemplate client) {
+        this.client = client;
+    }
+
     @Override
     public List<CmBookingDto> fetchBookings(LocalDate from, LocalDate to) {
         return List.of();
